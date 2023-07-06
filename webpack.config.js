00db74965418ded4893a28fa9da946d79da3e421
hyeonjun-L <beeminor60@gmail.com>
2023-07-07 00:01:08 +0900

    test

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,13 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        loader: "file-loader",
+        options: {
+          name: "[path][name].[ext]",
+        },
+      },
+      {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
